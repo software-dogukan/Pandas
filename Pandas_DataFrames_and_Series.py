5f29106c4c847c47f58bc01af96ad42e7d73df97
Dogukan Alkan <78702122+software-dogukan@users.noreply.github.com>
2024-07-20 23:58:55 +0300

    Data and Series

---
+++ Pandas_DataFrames_and_Series.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+my_data_set={'cars':["BMW","Volvo","Ford"],'passings':[3,7,2]}
+my_var=pd.DataFrame(my_data_set)
+
+print("*************************")
+data_set=[1,7,5]
+my_set=pd.Series(data_set)
+
+
+
